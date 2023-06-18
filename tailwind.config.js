3bbd6525637e545527496a45fef362223389724a
CamiloSinningUN <camilosinning.cs@gmail.com>
2023-06-18 00:56:00 -0500

    Set initial settings

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				lightBlue: '#5249ff'
+			},
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px'
+			}
+		}
+	},
+	plugins: []
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@fonts': path.resolve(__dirname, './src/assets/fonts'),
+			'@images': path.resolve(__dirname, './src/assets/images'),
+			'@styles': path.resolve(__dirname, './src/assets/styles'),
+			'@icons': path.resolve(__dirname, './src/assets/icons'),
+			'@hooks': path.resolve(__dirname, './src/core/hooks'),
+			'@utils': path.resolve(__dirname, './src/core/utils'),
+			'@locales': path.resolve(__dirname, './src/locales'),
+			'@sections': path.resolve(__dirname, './src/sections'),
+			'@components': path.resolve(__dirname, './src/shared/components')
+		}
+	}
+});
